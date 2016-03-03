dd453a640b8ee013f1b65985852cf8202b61757d
Igor <igkorovkin@yandex.ru>
2016-03-03 17:23:35 +0300

    notepad v.1, working

---
+++ memo.rb
@@ -0,0 +1,23 @@
+class Memo < Post
+
+  def read_from_console
+    puts "Новая заметка (всё, что пишется до строчки \"end\"):"
+
+    #Создаём массив для текста, вводимого пользователем
+    @text = []
+    line = nil
+
+    while line != "end" do
+      line = STDIN.gets.chomp
+      @text << line
+    end
+
+    @text.pop
+  end
+
+  def to_string
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    return @text.unshift(time_string)
+  end
+end
